208b019f5785ff06e586ad8bf03a91eea4ca0b97
“Adivhatkar” <adivhatkar@gmail.com>
2020-04-10 22:13:57 +0200

    Code cleaning

---
+++ index.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var app = new express();
+var path = require('path');
+app.use(express.static(path.join(__dirname, 'public')))
+app.listen(process.env.PORT || 4000, function(){
+    console.log('Your node js server is running at',process.env.PORT || 4000);
+});
